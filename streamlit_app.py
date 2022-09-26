36caabb212c5c6666a7403cb1390efee4419eb9a
Eduardo Olmos <eduardo@olmosgroup.com>
2022-09-26 15:57:21 -0700

    Update streamlit_app.py
    Added line item for avocado toast
    Added emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
 
 streamlit.title("My Parent's New Healthy Diner")
-streamlit.text("Omega 3 & Blueberry Oatmeal")
-streamlit.text("Kale, Spinach & Rocket Smoothie")
-streamlit.text("Hard-Boiled Free-Range Egg")
+
+streamlit.text("🥣 Omega 3 & Blueberry Oatmeal")
+streamlit.text("🥗 Kale, Spinach & Rocket Smoothie")
+streamlit.text("🐔 Hard-Boiled Free-Range Egg")
+streamlit.test("🥑🍞 Avocado Toast")
