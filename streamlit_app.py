1231d4b63bcbc7141b577893c9aad1f6ab4e9d87
Eduardo Olmos <eduardo@olmosgroup.com>
2022-09-27 13:18:53 -0700

    Update streamlit_app.py
    Updated the multiselect to filter the table.

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,6 +13,7 @@ streamlit.header("🍌🥭 Build Your Own Fruit Smoothie 🥝🍇")
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index("Fruit")
 
-streamlit.multiselect("Pick Some Fruits:", list(my_fruit_list.index), ["Avocado", "Strawberries"])
+selected_fruit = streamlit.multiselect("Pick Some Fruits:", list(my_fruit_list.index), ["Avocado", "Strawberries"])
+fruit_to_show = my_fruit_list.loc[selected_fruit]
 
-streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(fruit_to_show)
